3bda24af7f5e9d4b22814cc3313eace32cca0913
Reaganjade <164587187+Reaganjade@users.noreply.github.com>
2024-03-22 18:29:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+}
+
+
+let accordion = document.querySelectorAll('.Economy .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+   acco.onclick = () =>{
+      accordion.forEach(remove => remove.classList.remove('active'));
+      acco.classList.add('active');
+   }
+});
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
